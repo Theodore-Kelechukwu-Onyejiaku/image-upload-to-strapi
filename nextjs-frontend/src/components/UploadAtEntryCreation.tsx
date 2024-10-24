d895032c2e2e5ad3b7b7560f1706d4f0e9cd5374
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-10-24 17:23:02 +0100

    implement upload at entry creation

--- nextjs-frontend/src/components/UploadAtEntryCreation.tsx
+++ nextjs-frontend/src/components/UploadAtEntryCreation.tsx
@@ -1,14 +1,36 @@
-
 "use client";
-
+import { uploadAtEntryCreationAction } from "@/app/actions";
+import { useFormState } from "react-dom";
+import { useEffect, useRef } from "react";
 import SubmitButton from "./SubmitButton";
+import { toast } from "react-toastify";
+
+const initialState = {
+  uploadError: null,
+  uploadSuccess: null,
+};
 
 export default function UploadAtEntryCreation() {
+  const [state, formAction] = useFormState(
+    uploadAtEntryCreationAction,
+    initialState
+  );
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    formRef?.current?.reset();
+    toast.success(state?.uploadSuccess);
+  }, [state]);
+
   return (
-    <form className="flex rounded h-screen lg:w-full">
+    <form
+      ref={formRef}
+      action={formAction}
+      className="flex rounded h-screen lg:w-full"
+    >
       <div className="w-full">
         <div className="w-full my-5">
-          <p className=" text-base lg:text-lg font-semibold">
+          <p className=" text-base lg:text-lg">
             Upload a File at Entry Creation
           </p>
           <span className="text-sm text-[#71717a]">
@@ -39,6 +61,9 @@ export default function UploadAtEntryCreation() {
               Here, you can upload two or more files!
             </span>
           </span>
+          {state?.uploadError ? (
+            <span className="text-red-500">{state?.uploadError}</span>
+          ) : null}
           <SubmitButton title="Upload" />
         </div>
       </div>
