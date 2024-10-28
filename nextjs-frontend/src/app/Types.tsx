763c915136a90ef912221d6f511f90698ed29f2e
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-10-28 10:36:27 +0100

    update types

--- nextjs-frontend/src/app/Types.tsx
+++ nextjs-frontend/src/app/Types.tsx
@@ -1,14 +1,3 @@
-// food entry data types
-export interface FoodEntry {
-  id: number;
-  documentId: string;
-  name: string;
-  cover: {
-    name: string;
-    url: string;
-  };
-}
-
 // Image entry data types
 export interface ImageEntry {
   id: number;
@@ -17,4 +6,12 @@ export interface ImageEntry {
   caption: string;
   alternativeText: string;
   url: string;
+}
+
+// food entry data types
+export interface FoodEntry {
+  id: number;
+  documentId: string;
+  name: string;
+  cover: ImageEntry;
 }
